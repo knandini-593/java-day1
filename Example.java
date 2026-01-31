336f5fea053e48b50d061621854f53da7090d8e9
knandini-593 <kondappagarinandini@gmail.com>
2026-01-31 14:31:37 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example{
+    public static void main(String[] args) {
+      
+        System.out.print("Hello");
+        System.out.print("world");
+        
+     
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
