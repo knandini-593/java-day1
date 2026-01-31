e1ddd775038ab305c8e8b2b5d468227197be9764
knandini-593 <kondappagarinandini@gmail.com>
2026-01-31 16:08:10 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -2,16 +2,23 @@ import java.util.*;
 public class Test {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age:");
         int age=sc.nextInt();
+        System.out.println("Enter your a value:");
         double a=sc.nextDouble();
+        System.out.println("Enter your b value:");
         float b=sc.nextFloat();
+        System.out.println("Enter your str1 value:");
         String str1=sc.next();
-        String str2=sc.nextLine();
+        System.out.println("Enter your str2 value:");
+        String str2=sc.next();
+        System.out.println(age);
         System.out.println(a);
         System.out.println(b);
         System.out.println(str1);
-        System.out.println(age);
+        System.out.println(str2);
     }
     }
+    
     
 
