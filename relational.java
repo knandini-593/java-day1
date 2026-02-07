48f33240055e8c12426c99a5d7dc7fd9df54efbc
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 13:55:21 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,17 @@
+public class relational {
+    public static void main(String[] args){
+         double a=3.4;
+         double b=5.6;
+         System.out.println(a>b);
+          System.out.println(a<b);
+          System.out.println(a>=b);
+          System.out.println(a<=b);
+          System.out.println(a==b);
+          System.out.println(a!=b);
+          
+         
+
+
+    }
+    
+}
