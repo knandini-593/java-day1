852d24f8923f6bcc73dff44114b11f868d467146
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 11:04:24 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,14 @@
+public class details {
+    public static void main(String[] args){
+        int total;
+        String name;
+        int subject1=93;
+        int subject2=76;
+        int subject3=98;
+        total=subject1+subject2+subject3/3;
+        System.out.println(total);
+        System.out.println(name:"ravi");
+
+    }
+    
+}
