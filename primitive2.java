1ee9a909afc42b6f5fcf70c5170dd59440695ae3
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 10:53:07 +0530

    Add files via upload

---
+++ primitive2.java
@@ -0,0 +1,21 @@
+public class primitive2 {
+    public static void main(String[] args){
+        byte b=56;
+        short s=4567;
+        int a=67;
+        long l=4942065;
+        float f=5484.f;
+        double d=7.65489;
+        char c='g';
+        boolean boo=true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(boo);
+    }
+}
