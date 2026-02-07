078772b22e9fca0a23bb598c9da399049b23d8e4
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 14:36:33 +0530

    Add files via upload

---
+++ All.java
@@ -0,0 +1,21 @@
+public class All {
+    public static void main(String[] args){
+        int a=20;
+        int b=10;
+        int sum=a+b;
+        System.out.println(sum);
+        if(0>1000){
+                System.out.println("Number Is Even");
+        }
+        else{
+            System.out.println("Number Is Negative");
+        }
+        if(a%2==0){
+                System.out.println("Number Is Positive And Even");
+        }
+        System.out.println(a);
+        System.out.println(a++);
+        System.out.println(a);
+    }
+    
+}
