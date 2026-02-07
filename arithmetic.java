f9be9a9219fd22259968fd3672373f6de23ebfd5
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 15:12:23 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,14 @@
+public class arithmetic {
+    public static void main(String[] args){
+        int a=87;
+        int b=67;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println();
+         
+    }
+    
+}
