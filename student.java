e15d14a0bd13858290a18b8ec178a5b42feb0ea0
knandini-593 <kondappagarinandini@gmail.com>
2026-02-07 13:49:02 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+public class student{
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+        System.out.println(a+b);
+        int sum=a+b;
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(sum);
+        
+
+    }
+}
